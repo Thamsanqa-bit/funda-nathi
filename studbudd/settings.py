d3641971cabbb78092e725e37cb3af9084b74b18
Thamsanqa Mbodla <thamsanqambodla@gmail.com>
2023-10-13 10:15:47 +0200

    removed environ

--- studbudd/settings.py
+++ studbudd/settings.py
@@ -16,10 +16,10 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import environ
+# import environ
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
